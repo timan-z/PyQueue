7c8f839dd3313803bea20a4a9d5f37937e941a03
Timan Zheng <timanz100@gmail.com>
2026-01-30 20:28:09 -0500

    FastAPI Idiomatic Refactoring Phase #3 - Changing modes/task.py to be a dataclass

--- models/task.py
+++ models/task.py
@@ -1,14 +1,27 @@
 import datetime
+from dataclasses import dataclass
 from enums.TaskStatus import TaskStatus
 from enums.TaskType import TaskType
 
+@dataclass
+class Task:
+    t_id: str
+    payload: str
+    t_type: TaskType
+    status: TaskStatus
+    attempts: int
+    max_retries: int
+    created_at: datetime.datetime
+
+# Phase 1-2 (pre-@dataclass introduction) legacy code:
+"""
 class Task:
     # Had a realization that when I instantiate new Tasks in SpringQueue, SpringQueuePro, and GoQueue
     # I'm only actually providing the payload and TaskType (everything else is provided and determined by the system).
     # The (original) __init__ constructor I have below is more of a storage model than a creation model:
     # EDIT: ^ nvm I don't know what I'm talking about -- but maybe I should adjust this a bit to delegate multiple
     # constructors to @classmethod (Python only allows for one constructor normally but there are workarounds).
-
+    
     def __init__(self, t_id: str, payload: str, t_type: TaskType, status: TaskStatus, attempts: int, max_retries: int, created_at: datetime.datetime):
         self.t_id = t_id
         self.payload = payload
@@ -17,3 +30,4 @@ class Task:
         self.attempts = attempts
         self.max_retries = max_retries
         self.created_at = created_at
+"""
