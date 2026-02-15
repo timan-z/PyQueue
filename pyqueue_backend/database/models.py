2e3bdaa3fabcf137c353b54933b3c7506e7ed5d4
Timan Zheng <timanz100@gmail.com>
2026-02-14 21:27:14 -0500

    SQLAlchemy + MySQL Config Phase #3 - Wrote a test file to play around w/ inserting actual Test ORM model reps into my local MySQL server + playing w/ Rollback raising errors etc. Added __repr__ string method too.

--- pyqueue_backend/database/models.py
+++ pyqueue_backend/database/models.py
@@ -10,3 +10,18 @@ class Task(Base):
     attempts = Column(Integer, nullable=False, default = 0) # Defaults here and below protect data integrity.
     max_retries = Column(Integer, nullable=False, default = 3)
     created_at = Column(DateTime, nullable=False, index=True, server_default=func.now())
+
+    # Method below is for defining a developer-facing string rep of an object.
+    def __repr__(self):
+        return (
+            f"<Task(id={self.id!r}, "   # {self.id!r} calls repr() on the value.
+            f"status={self.status!r}, "
+            f"task_type={self.task_type!r}, "
+            f"attempts={self.attempts!r}, "
+            f"max_retries={self.max_retries!r}, "
+            f")>"
+        )
+
+    @property
+    def is_retryable(self):
+        return self.attempts >= self.max_retries
